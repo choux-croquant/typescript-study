54b5685dd2dc8fcd8d9152b3476127141d40dc19
Choux <choifrance@gmail.com>
2022-04-26 23:50:22 +0900

    allocate type on this keyword

--- class-and-interface/src/app.ts
+++ class-and-interface/src/app.ts
@@ -4,8 +4,16 @@ class ItemList {
   constructor(n: string) {
     this.name = n;
   }
+  // this에 더미 타입을 추가하는 것을 통해 외부에서 유사한 객체가 존재할 때 발생할 수 있는 에러에 대해 어느정도 대책을 세울 수 있다.
+  consoleListName(this: ItemList) {
+    console.log('ListName: ' + this.name)
+  }
 }
 
 const myFirstItemList = new ItemList('FirstItemList')
+const someItemList = { consoleListName: myFirstItemList.consoleListName}
 
-console.log(myFirstItemList)
+myFirstItemList.consoleListName()
+// this에 타입을 지정하지 않았다면 아래의 메서드는 정상적으로 동작하는 것 처럼 보인다.
+// 현재는 name속성도 추가하여 ItemList의 형식과 동일하게 만들어주어야 동작한다.
+someItemList.consoleListName()
